b0fd8a408491a9ea6e9123cd9a78ff36c6c042bc
igoreta <ivoronline@gmail.com>
2021-02-23 00:26:40 +0100

    Initial commit

---
+++ src/main/java/com/example/springboot_demo_store_entity_from_json/SpringbootDemoStoreEntityFromJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_demo_store_entity_from_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoStoreEntityFromJsonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDemoStoreEntityFromJsonApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/example/springboot_demo_store_entity_from_json/controllers/MyController.java
@@ -0,0 +1,30 @@
+package com.example.springboot_demo_store_entity_from_json.controllers;
+
+import com.example.springboot_demo_store_entity_from_json.entities.Person;
+import com.example.springboot_demo_store_entity_from_json.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+  @Autowired PersonRepository personRepository;
+
+  @ResponseBody
+  @RequestMapping("/AddPerson")
+  public String addPerson(@RequestBody Person person) {
+
+    //STORE PERSON ENTITY
+    personRepository.save(person);
+
+    //RETURN SOMETHING TO BROWSER
+    return "Person added to DB";
+
+  }
+
+}
+
+
